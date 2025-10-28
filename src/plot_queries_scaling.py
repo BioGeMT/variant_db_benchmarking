19252c155f0fbbb6a23e533914122cc1493ecabc
Andrea Gariboldi <andrea.gariboldi@um.edu.mt>
2025-10-28 16:15:28 +0100

    Add query4 data and scaling plots

--- src/plot_queries_scaling.py
+++ src/plot_queries_scaling.py
@@ -4,7 +4,7 @@ import scienceplots
 
 plt.style.use('science')
 
-queries = ['query1', 'query2', 'query3']
+queries = ['query1', 'query2', 'query3', 'query4']
 
 for query in queries:
     df = pd.read_csv(f'data/{query}_subsamples.csv')
